c32d14d7c07e808cf72cc0dcf7fc6a6e2cf014b0
Vitaly <itassan6@gmail.com>
2022-11-23 12:29:44 +0200

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
 import { useEffect } from "react";
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-import { useAppDispatch } from './hooks/redux'
-import { fetchCountries } from './store/reducers/ActionCreators'
+import { useAppDispatch } from "./hooks/redux";
+import { fetchCountries } from "./store/reducers/ActionCreators";
 
-import Header from "./components/Header"
-import HomePage from './pages/HomePage'
-import CountryPage from './pages/CountryPage'
-
-
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import CountryPage from "./pages/CountryPage";
 
 function App() {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(fetchCountries())
+    dispatch(fetchCountries());
     // eslint-disable-next-line
-  },[])
+  }, []);
 
   return (
-    <div className='app'>
+    <div className="app">
       <Header />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/country/:name" element={<CountryPage />} />  
+        <Route path="" element={<HomePage />} />
+        <Route path="country/:name" element={<CountryPage />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
